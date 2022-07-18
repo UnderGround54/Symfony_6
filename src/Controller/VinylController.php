486952d4f9ae9566197787724076ef8e9e6aff65
UnderGround54 <martinpros54@gmail.com>
2022-07-18 16:32:52 +0300

    Creer templates twig

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,14 +2,25 @@
 namespace App\Controller;
 
 use function Symfony\Component\String\u;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class VinylController {
+class VinylController extends AbstractController {
 
     #[Route('/')]
     public function homepage(): Response {
-        return new Response('Author: Bob Marley');
+
+        $tracks = [
+            ['Artist' => 'Ramora', 'Titre' => 'President'],
+            ['Artist' => 'Mijah ', 'Titre' => ' mifoka miafina'],
+            ['Artist' => 'Bot ', 'Titre' => ' Rapiera'],
+            ['Artist' => 'Bolo pix ', 'Titre' => 'On my way'],
+        ];
+        return $this->render('pages/homepage.html.twig',[
+            'Title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
     }
 
     #[Route('/browse/{slug}')]
