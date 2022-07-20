396386478dbf0eb4eb897bf496455236f8f8e956
UnderGround54 <martinpros54@gmail.com>
2022-07-20 10:12:39 +0300

    configurer log (journal)

--- src/Controller/MusicsApiController.php
+++ src/Controller/MusicsApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,13 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MusicsApiController extends AbstractController{
 
-    #[Route('/api/musics/{id}')]
-    public function getMusics($id): Response {
+    #[Route('/api/musics/{id<\d+>}', methods: ['GET'])]
+    public function getMusics(int $id, LoggerInterface $logger): Response {
         $musics = [
             'id' => $id,
             'name' => 'Music Slow',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3'
         ];
+
+        $logger->info('api symfony 5 {musics}', [ 
+            'musics' => $id,
+        ]);
         // return new JsonResponse($musics);
         return $this->json($musics);
     }
