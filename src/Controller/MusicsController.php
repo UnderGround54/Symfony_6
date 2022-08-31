f76ca66cb6150b63c5b0c02b08c6e9707e69986d
UnderGround54 <martinpros54@gmail.com>
2022-08-31 10:42:05 +0300

    installation bundle time

--- src/Controller/MusicsController.php
+++ src/Controller/MusicsController.php
@@ -8,19 +8,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MusicsController extends AbstractController {
 
-    public $tracks = [
-        ['Artist' => 'Ramora', 'Titre' => 'President', 'Genre' => 'Pop'],
-        ['Artist' => 'Mijah ', 'Titre' => ' mifoka miafina', 'Genre' => 'Rock'],
-        ['Artist' => 'DA 7', 'Titre' => ' Rapiera', 'Genre' => 'Slow'],
-        ['Artist' => 'Bolo pix ', 'Titre' => 'On my way', 'Genre' => 'K-POP'],
-    ];
+    // public $tracks = [
+    //     ['Artist' => 'Ramora', 'Titre' => 'President', 'Genre' => 'Pop'],
+    //     ['Artist' => 'Mijah ', 'Titre' => ' mifoka miafina', 'Genre' => 'Rock'],
+    //     ['Artist' => 'DA 7', 'Titre' => ' Rapiera', 'Genre' => 'Slow'],
+    //     ['Artist' => 'Bolo pix ', 'Titre' => 'On my way', 'Genre' => 'K-POP'],
+    // ];
 
     #[Route('/', name: 'home')]
     public function homepage(): Response {
         // dump($tracks);
         return $this->render('pages/homepage.html.twig',[
             'Title' => 'Hira Malagasy',
-            'tracks' => $this->tracks,
+            'tracks' => $this->getMixes(),
         ]);
     }
 
@@ -29,11 +29,22 @@ class MusicsController extends AbstractController {
     {
         
         $genre = $recherche ? u(str_replace('_',' ', $recherche))->title(true) : null;
-       
+        $tracks = $this->getMixes();
+
         return $this->render('pages/browse.html.twig',[
             'genre' => $genre,
-            'tracks' => $this->tracks,
+            'tracks' => $tracks,
         ]);
+    }
+
+    private function getMixes(): array
+    {
+        return [
+            ['Artist' => 'Ramora', 'Titre' => 'President', 'Genre' => 'Pop', 'createdAt' => new \DateTime('12-01-2016')],
+            ['Artist' => 'Mijah ', 'Titre' => ' mifoka miafina', 'Genre' =>'Rock', 'createdAt' => new \DateTime('02-09-2020')],
+            ['Artist' => 'DA 7', 'Titre' => ' Rapiera', 'Genre' =>'Slow', 'createdAt' => new \DateTime('14-12-2021')],
+            ['Artist' => 'Bolo pix ', 'Titre' => 'On my way', 'Genre' => 'K-POP', 'createdAt' => new \DateTime('17-03-2020')],
+        ];
     }
 
 }
